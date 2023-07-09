9fbd996f05e74b1f776803268768738d81a0469d
Ushira Dineth <ushiradineth@gmail.com>
2023-07-09 21:27:12 +0530

    feat(e2e): added cypress testing

---
+++ cypress.config.ts
@@ -0,0 +1,20 @@
+import { loadEnvConfig } from "@next/env";
+import { defineConfig } from "cypress";
+
+const { combinedEnv } = loadEnvConfig(process.cwd());
+
+export default defineConfig({
+  env: combinedEnv,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+});
