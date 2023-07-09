a647d0d60ee54083ccb0dc509bf858f86a84c461
Ushira Dineth <ushiradineth@gmail.com>
2023-07-09 20:38:11 +0530

    feat(tailwind): added tailwind design system

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,36 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+
 import { type AppType } from "next/app";
+
 import { api } from "@/utils/api";
+
 import "@/styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+import { DM_Sans, Libre_Baskerville } from "@next/font/google";
+
+const dmsans = DM_Sans({
+  weight: ["700"],
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+const libre = Libre_Baskerville({
+  weight: ["700"],
+  subsets: ["latin"],
+  variable: "--font-libre",
+});
+
+const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <SessionProvider session={session}>
+      <style jsx global>
+        {`
+          :root {
+            --font-sans: ${dmsans.style.fontFamily};
+            --font-libre: ${libre.style.fontFamily};
+          }
+        `}
+      </style>
       <Component {...pageProps} />
     </SessionProvider>
   );
