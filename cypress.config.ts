7c5f79aab658a52715f2dcfbe790dab4893d52b0
Ushira Dineth <ushiradineth@gmail.com>
2023-07-16 20:44:37 +0530

    test: cypress testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:3000",
-  },
-
   component: {
     devServer: {
       framework: "next",
       bundler: "webpack",
     },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {},
   },
 });
